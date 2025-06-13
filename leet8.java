ced2e8985a98b9d9355232709776e5d65b92e643
TheFletSher <lfhsherson95@gmail.com>
2025-06-13 16:32:19 -0400

    2 checks left #8

--- leet8.java
+++ leet8.java
@@ -1,17 +1,20 @@
 public class leet8 {
     public int myAtoi(String s) {
         int result = 0;
+        double max = Integer.MAX_VALUE / 10;
         
-        boolean sign = true, checkSign = true;
+        boolean sign = true, checking = true;
         for (char c : s.toCharArray()) {
-            if (c == ' ') {
+            if (c == ' ' && checking) {
                 continue;
-            } else if (c =='-' && checkSign) {
+            } else if (c =='-' && checking) {
                 sign = false;
+                checking = false;
+            } else if (c == '+' && checking) {
+                checking = false;
             } else if (Character.isDigit(c)) {
-                checkSign = false;
-                int newNum = result * 10 + (c - '0');
-                if (newNum < result) {
+                checking = false;
+                if (result >= max) {
                     return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
                 }
                 result = result * 10 + (c - '0');
