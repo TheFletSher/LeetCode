813eadb7b4754d77f7e60849612c480f91a65b7d
TheFletSher <lfhsherson95@gmail.com>
2025-06-13 16:55:10 -0400

    much better solution #8

--- leet8.java
+++ leet8.java
@@ -5,42 +5,68 @@ public class leet8 {
     }
 
     public static int myAtoi(String s) {
-        int result = 0;
-        int max = Integer.MAX_VALUE / 10;
-        
-        boolean sign = true, checking = true;
-        for (char c : s.toCharArray()) {
-            if (checking && c == ' ') {
-                continue;
-            } else if (checking && c =='-') {
-                sign = false;
-                checking = false;
-            } else if (checking && c == '+') {
-                checking = false;
-            } else if (Character.isDigit(c)) {
-                checking = false;
-                int digit = c - '0';
-                if (result >= max) {
-                    if (result == max && digit <= 8) {
-                        if (sign && digit >= 7) {
-                            return Integer.MAX_VALUE;
-                        } else if (!sign && digit == 8) {
-                            return Integer.MIN_VALUE;
-                        } else {
-                            result = result * 10 + digit;
-                        }
-                    } else {
-                        return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                    }
-                } else {
-                    result = result * 10 + digit;
-                }
-            } else {
-                System.out.println("BREAK");
-                break;
+        s = s.trim();
+        long result = 0;
+        if (s.length() == 0) {
+            return 0;
+        }
+
+        int index = 0, sign = 1;
+
+        if (s.charAt(index) == '-' || s.charAt(index) == '+') {
+            sign = s.charAt(index++) == '-' ? -1 : 1;
+        }
+
+        while (index < s.length() && Character.isDigit(s.charAt(index))) {
+            result = result * 10 + (s.charAt(index++) - '0') * sign;
+            if (result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
             }
         }
 
-        return result = sign ? result : result * -1;
+        return (int) result;
     }
+
+    // public static int myAtoi(String s) {
+    //     int result = 0;
+    //     int max = Integer.MAX_VALUE / 10;
+        
+    //     boolean sign = true, checking = true;
+    //     for (char c : s.toCharArray()) {
+    //         if (checking && c == ' ') {
+    //             continue;
+    //         } else if (checking && c =='-') {
+    //             sign = false;
+    //             checking = false;
+    //         } else if (checking && c == '+') {
+    //             checking = false;
+    //         } else if (Character.isDigit(c)) {
+    //             checking = false;
+    //             int digit = c - '0';
+    //             if (result >= max) {
+    //                 if (result == max && digit <= 8) {
+    //                     if (sign && digit >= 7) {
+    //                         return Integer.MAX_VALUE;
+    //                     } else if (!sign && digit == 8) {
+    //                         return Integer.MIN_VALUE;
+    //                     } else {
+    //                         result = result * 10 + digit;
+    //                     }
+    //                 } else {
+    //                     return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+    //                 }
+    //             } else {
+    //                 result = result * 10 + digit;
+    //             }
+    //         } else {
+    //             System.out.println("BREAK");
+    //             break;
+    //         }
+    //     }
+
+    //     return result = sign ? result : -result;
+    // }
 }
