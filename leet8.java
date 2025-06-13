9893b2e06049d02d19b79cded448bb33072e6ba6
TheFletSher <lfhsherson95@gmail.com>
2025-06-13 16:47:31 -0400

    bad solution #8

--- leet8.java
+++ leet8.java
@@ -1,24 +1,42 @@
 public class leet8 {
-    public int myAtoi(String s) {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println(myAtoi("-21474836482"));
+    }
+
+    public static int myAtoi(String s) {
         int result = 0;
-        double max = Integer.MAX_VALUE / 10;
+        int max = Integer.MAX_VALUE / 10;
         
         boolean sign = true, checking = true;
         for (char c : s.toCharArray()) {
-            if (c == ' ' && checking) {
+            if (checking && c == ' ') {
                 continue;
-            } else if (c =='-' && checking) {
+            } else if (checking && c =='-') {
                 sign = false;
                 checking = false;
-            } else if (c == '+' && checking) {
+            } else if (checking && c == '+') {
                 checking = false;
             } else if (Character.isDigit(c)) {
                 checking = false;
+                int digit = c - '0';
                 if (result >= max) {
-                    return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                    if (result == max && digit <= 8) {
+                        if (sign && digit >= 7) {
+                            return Integer.MAX_VALUE;
+                        } else if (!sign && digit == 8) {
+                            return Integer.MIN_VALUE;
+                        } else {
+                            result = result * 10 + digit;
+                        }
+                    } else {
+                        return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                    }
+                } else {
+                    result = result * 10 + digit;
                 }
-                result = result * 10 + (c - '0');
             } else {
+                System.out.println("BREAK");
                 break;
             }
         }
