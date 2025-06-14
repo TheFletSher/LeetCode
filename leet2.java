4f19c2c34b697c6e67cf71c5c895166ea6b6a373
TheFletSher <lfhsherson95@gmail.com>
2025-06-14 19:53:11 -0400

    not sure why this doesn't work tbh #2

--- leet2.java
+++ leet2.java
@@ -1,7 +1,7 @@
 public class leet2 {
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3)));
-        ListNode l2 = new ListNode(4, new ListNode(5, new ListNode(6)));
+        ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        ListNode l2 = new ListNode(9, new ListNode(9, new ListNode(9)));
 
         int num1 = 0, exp = 0;
         ListNode curr = l1;
@@ -10,7 +10,15 @@ public class leet2 {
             curr = curr.next;
         }
 
-        System.out.println(num1);
+        ListNode result = addTwoNumbers(l1, l2);
+
+        System.out.println();
+
+        curr = result;
+        while (curr != null) {
+            System.out.println(curr.val);
+            curr = curr.next;
+        }
     }
 
     public static class ListNode {
@@ -21,7 +29,7 @@ public class leet2 {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int num1 = 0, num2 = 0, exp = 0;
 
         ListNode curr = l1;
@@ -30,6 +38,8 @@ public class leet2 {
             curr = curr.next;
         }
 
+        System.out.println(num1);
+
         exp = 0;
         curr = l2;
         while (curr != null) {
@@ -37,14 +47,22 @@ public class leet2 {
             curr = curr.next;
         }
 
+        System.out.println(num2);
+
         exp = 0;
         int total = num1 + num2;
+
+        System.out.println(total);
+
         ListNode result = new ListNode(total % 10);
         total /= 10;
         curr = result;
         while (total > 0) {
-            curr.next = new ListNode(total % 10);
+            int val = total % 10;
+            curr.next = new ListNode(val);
+            System.out.println(total % 10);
             total /= 10;
+            System.out.println(total);
             curr = result.next;
         }
 
