f509a3d5588996f12199da6f5ceb4bf1f3674720
TheFletSher <lfhsherson95@gmail.com>
2025-06-14 18:50:49 -0400

    finished solution 2566

--- leet2566.java
+++ leet2566.java
@@ -1,17 +1,22 @@
 public class leet2566 {
-    public int minMaxDifference(int num) {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println(minMaxDifference(11891));
+    }
+
+    public static int minMaxDifference(int num) {
         String digits = Integer.toString(num);
         int length = digits.length();
         
-        int min = Integer.valueOf(digits.substring(1, length));
+        int min = Integer.valueOf(digits.replace(Character.toString(digits.charAt(0)), "0"));
         for (int i = 0; i < length; i++) {
             char curr = digits.charAt(i);
             if (curr != '9') {
-                digits.replaceFirst(Character.toString(curr), "9");
+                digits = digits.replace(Character.toString(curr), "9");
                 return Integer.valueOf(digits) - min;
             }
         }
         
-        return num - min - 1;
+        return num - min;
     }
 }
