fc8c87a641e5ff425844b1e9e34ee68fdf107d9b
TheFletSher <lfhsherson95@gmail.com>
2025-06-14 18:45:55 -0400

    possible solution 2566

--- leet2566.java
+++ leet2566.java
@@ -1,8 +1,17 @@
 public class leet2566 {
     public int minMaxDifference(int num) {
-        String digits = String.valueOf(num);
+        String digits = Integer.toString(num);
+        int length = digits.length();
         
+        int min = Integer.valueOf(digits.substring(1, length));
+        for (int i = 0; i < length; i++) {
+            char curr = digits.charAt(i);
+            if (curr != '9') {
+                digits.replaceFirst(Character.toString(curr), "9");
+                return Integer.valueOf(digits) - min;
+            }
+        }
         
-
+        return num - min - 1;
     }
 }
