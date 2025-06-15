00d44a82f3bfeb3b827007a7680041937ccbb03b
TheFletSher <lfhsherson95@gmail.com>
2025-06-14 21:27:55 -0400

    problem 9 no string

--- leet9.java
+++ leet9.java
@@ -1,23 +1,43 @@
 public class leet9 {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(121));
     }
 
     public static boolean isPalindrome(int x) {
-        String num = String.valueOf(x);
-        System.out.println(num);
-        
-        int left = 0;
-        int right = num.length() - 1;
+        if (x < 0) {
+            return false;
+        }
 
-        while (right - left <= 1) {
-            System.out.println(num.charAt(left) + ", " + num.charAt(right));
-            if (num.charAt(left) != num.charAt(right)) {
+        if (x < 10) {
+            return true;
+        } else if (x < 100) {
+            if (x / 10 == x % 10) {
+                return true;
+            }
+            return false;
+        }
+        
+        int exp = 0;
+
+        int total = x;
+
+        while (total > 0) {
+            total /= 10;
+            exp++;
+        }
+
+        exp--;
+
+        int smallest = x / (int) Math.pow(10, (exp / 2) + 1);
+        while (x > smallest) {
+            int left = (x / (int) Math.pow(10, exp)) % 10;
+            int right = x % 10;
+            if (left != right) {
                 return false;
             }
-            left++;
-            right--;
+            x /= 10;
+            exp -= 2;
         }
 
         return true;
