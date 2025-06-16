6494c3333a7abe80309b37e7a69668a35ae4fe63
TheFletSher <lfhsherson95@gmail.com>
2025-06-16 14:45:13 -0400

    1432 string solution

--- leet1432.java
+++ leet1432.java
@@ -1,5 +1,40 @@
 public class leet1432 {
-    public int maxDiff(int num) {
-        
+    public static void main(String[] args) {
+        int num = 111;
+        System.out.println();
+        System.out.println(maxDiff(num));
+    }
+
+    public static int maxDiff(int num) {
+        if (num < 10) {
+            return 8;
+        }
+
+        String max = Integer.toString(num);
+
+        String min = "0";
+
+        if (max.charAt(0) != '1') {
+            min = max.replace(Character.toString(max.charAt(0)), "1");
+        } else {
+            for (int i = 1; i < max.length(); i++) {
+                if (max.charAt(i) != '0' && max.charAt(i) != max.charAt(0)) {
+                    min = max.replace(Character.toString(max.charAt(i)), "0");
+                    break;
+                }
+            }
+            if (min.equals("0")) {
+                min = max;
+            }
+        }
+
+        for (int i = 0; i < max.length(); i++) {
+            if (max.charAt(i) != '9') {
+                max = max.replace(Character.toString(max.charAt(i)), "9");
+                break;
+            }
+        }
+
+        return Integer.valueOf(max) - Integer.valueOf(min);
     }
 }
