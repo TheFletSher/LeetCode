5d4d3ae619d509d4dbb6a7f177a0eb59506ee593
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 16:54:47 -0400

    cleaner

--- leet2966.java
+++ leet2966.java
@@ -5,8 +5,7 @@ public class leet2966 {
         int[][] result = new int[nums.length / 3][3];
 
         for (int i = 0; i < nums.length; i++) {
-            int current = i / 3;
-            result[current][i % 3] = nums[i];
+            result[i / 3][i % 3] = nums[i];
             if (i % 3 == 0 && nums[i] + k < nums[i + 2]) {
                 return new int[0][0];
             }
