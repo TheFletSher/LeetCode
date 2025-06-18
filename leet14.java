787c2ae17adc159bce96e7ab47e0195125677bf8
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 19:10:59 -0400

    startup for 14

--- leet14.java
+++ leet14.java
@@ -1,5 +1,16 @@
 public class leet14 {
     public String longestCommonPrefix(String[] strs) {
-        
+        String min = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            if (min == null || strs[i].length() < min.length()) {
+                min = strs[i];
+            }
+        }
+        StringBuilder prefix = new StringBuilder(min.charAt(0));
+        for (int i = 0; i < strs.length; i++) {
+            for (int j = 1; j < min.length(); j++) {
+                
+            }
+        }
     }
 }
