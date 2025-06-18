a665c91f58d0bdd2681517f725b270d8bc29419a
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 18:21:33 -0400

    good solution for 13

--- leet13.java
+++ leet13.java
@@ -1,5 +1,36 @@
 public class leet13 {
     public int romanToInt(String s) {
-        
+        int total = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c == 'I') {
+                if (total > 4) {
+                    total--;
+                } else {
+                    total++;
+                }
+            } else if (c == 'V') {
+                total += 5;
+            } else if (c == 'X') {
+                if (total > 30) {
+                    total -= 10;
+                } else {
+                    total += 10;
+                }
+            } else if (c == 'L') {
+                total += 50;
+            } else if (c == 'C') {
+                if (total > 300) {
+                    total -= 100;
+                } else {
+                    total += 100;
+                }
+            } else if (c == 'D') {
+                total += 500;
+            } else if (c == 'M') {
+                total += 1000;
+            }
+        }
+        return total;
     }
 }
