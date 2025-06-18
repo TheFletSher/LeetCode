9aca8e8c893570114f0819372bda26b46875d895
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 19:26:45 -0400

    working solution

--- leet14.java
+++ leet14.java
@@ -1,26 +1,27 @@
-import java.util.HashSet;
-
 public class leet14 {
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 1) {
             return strs[0];
         }
 
-        String min = strs[0];
+        // Find minimum string length
+        int min = strs[0].length();
         for (int i = 1; i < strs.length; i++) {
-            if (strs[i].length() < min.length()) {
-                min = strs[i];
+            if (strs[i].length() < min) {
+                min = strs[i].length();
             }
         }
+
+        // Go through each string checking same index for same character
         StringBuilder prefix = new StringBuilder("");
-        HashSet<Character> chars = new HashSet<>();
         int index = 0;
-        while (index < min.length()) {
-            chars.add(strs[0].charAt(index));
+        while (index < min) {
+            char c = strs[0].charAt(index);
             for (int i = 1; i < strs.length; i++) {
-                if (chars.add(strs[i].charAt(index))) return prefix.toString();
+                if (strs[i].charAt(index) != c) return prefix.toString();
             }
-            prefix.append(strs[0].charAt(index++));
+            prefix.append(c);
+            index++;
         }
 
         return prefix.toString();
