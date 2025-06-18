cc81e83a6dd7335134d15d180dd2a07b3a82f104
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 16:26:25 -0400

    starting 2966

---
+++ leet3405.java
@@ -0,0 +1,8 @@
+public class leet3405 {
+    public int countGoodArrays(int n, int m, int k) {
+        if (m == 2 && k == 0) {
+            return 2;
+        }
+        int[] arr = new int[n];
+    }
+}
