fc1acbbaf1886887d50dda6e60472cc584f33a6f
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 16:50:53 -0400

    more optimized slow solution 2966

--- leet2966.java
+++ leet2966.java
@@ -2,15 +2,11 @@ import java.util.Arrays;
 
 public class leet2966 {
     public int[][] divideArray(int[] nums, int k) {
-        Arrays.sort(nums);
-
         int[][] result = new int[nums.length / 3][3];
 
-        int index = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (index == 3) index = 0;
             int current = i / 3;
-            result[current][index++] = nums[i];
+            result[current][i % 3] = nums[i];
             if (i % 3 == 0 && nums[i] + k < nums[i + 2]) {
                 return new int[0][0];
             }
