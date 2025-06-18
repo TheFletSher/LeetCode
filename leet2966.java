67d6c7c86a47ebccd85d09f0bd795c1d4bb40b3f
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 16:57:53 -0400

    better solution

--- leet2966.java
+++ leet2966.java
@@ -5,8 +5,10 @@ public class leet2966 {
         Arrays.sort(nums);
         int[][] result = new int[nums.length / 3][3];
 
-        for (int i = 0; i < nums.length; i++) {
-            result[i / 3][i % 3] = nums[i];
+        for (int i = 0; i < nums.length; i+=3) {
+            result[i / 3][0] = nums[i];
+            result[i / 3][1] = nums[i + 1];
+            result[i / 3][2] = nums[i + 2];
             if (i % 3 == 0 && nums[i] + k < nums[i + 2]) {
                 return new int[0][0];
             }
