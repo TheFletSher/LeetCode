c969e3dd751b58944c410892206310cad64d9461
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 19:23:05 -0400

    first full attempt

--- leet14.java
+++ leet14.java
@@ -1,16 +1,28 @@
+import java.util.HashSet;
+
 public class leet14 {
     public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
         String min = strs[0];
         for (int i = 1; i < strs.length; i++) {
-            if (min == null || strs[i].length() < min.length()) {
+            if (strs[i].length() < min.length()) {
                 min = strs[i];
             }
         }
-        StringBuilder prefix = new StringBuilder(min.charAt(0));
-        for (int i = 0; i < strs.length; i++) {
-            for (int j = 1; j < min.length(); j++) {
-                
+        StringBuilder prefix = new StringBuilder("");
+        HashSet<Character> chars = new HashSet<>();
+        int index = 0;
+        while (index < min.length()) {
+            chars.add(strs[0].charAt(index));
+            for (int i = 1; i < strs.length; i++) {
+                if (chars.add(strs[i].charAt(index))) return prefix.toString();
             }
+            prefix.append(strs[0].charAt(index++));
         }
+
+        return prefix.toString();
     }
 }
