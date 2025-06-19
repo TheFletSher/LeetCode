90edfe13e451c103a0170504a579be74d76b75c0
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 21:12:43 -0400

    good start

--- leet15.java
+++ leet15.java
@@ -16,6 +16,7 @@ public class leet15 {
 
         int zero = 0;
 
+        List<List<Integer>> pairs = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
@@ -26,21 +27,24 @@ public class leet15 {
                 zero++;
             }
         }
-
-        List<List<Integer>> pairs = new ArrayList<>();
+        
+        if (zero >= 3) pairs.add(new ArrayList<>(List.of(0, 0, 0)));
+        if (pos.isEmpty() || neg.isEmpty()) return pairs;
 
         for (int num : neg.values()) {
             if (pos.containsKey(-num) && zero != 0) {
                 pairs.add(new ArrayList<>(List.of(num, 0, -num)));
             } else {
                 num = -num;
-                for (int i = 0; i < num; i++) {
-                    
+                for (int i = 1; i < num / 2; i++) {
+                    if (pos.containsKey(i) && pos.containsKey(num - i)) {
+                        pairs.add(new ArrayList<>(List.of(num, i, num - i)));
+                        System.out.println(num + ", " + i + ", " + (num - i));
+                    }
                 }
             }
         }
 
-        if (zero >= 3) pairs.add(new ArrayList<>(List.of(0, 0, 0)));
 
         return pairs;
     }
