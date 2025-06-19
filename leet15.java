4932d049598cb2f27c2fbac79911dca52be15ad6
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 20:09:42 -0400

    continued 15

--- leet15.java
+++ leet15.java
@@ -5,29 +5,36 @@ import java.util.List;
 public class leet15 {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int left = 0, right = nums.length - 1, total = 0;
+        int left = 0, right = nums.length - 1;
         // No negative numbers or zeros
         if (nums[right] + nums[left] > nums[right]) return new ArrayList<>(0);
 
-        ArrayList<ArrayList<Integer>> pairs = new ArrayList<>();
+        List<List<Integer>> pairs = new ArrayList<>();
 
         // Loop through for pairs
         while (right - left > 1) {
             // number to find
-            int need = - (nums[right] - nums[left]);
-            // can't work
+            int need = -1 * (nums[right] - nums[left]);
+            // can't work without duplicates
             if (need >= nums[right]) {
                 if (right - 1 != left && nums[right - 1] == need) {
                     pairs.add(new ArrayList<>(List.of(nums[left], nums[right - 1], nums[right])));
+                    while (nums[right] == need) {
+                        right--;
+                        if (right <= left) return pairs;
+                    }
                 }
-                right-=2;
                 left++;
             } else if (need <= nums[left]) {
                 if (left + 1 != right && nums[left + 1] == need) {
                     pairs.add(new ArrayList<>(List.of(nums[left], nums[left + 1], nums[right])));
+                    while (nums[left] == need) {
+                        left++;
+                        if (left >= right) return pairs;
+                    }
                 }
-                left+=2;
                 right--;
+            // Look for needed number between 
             } else {
                 for (int i = left; i < right - 1; i++) {
                     if (nums[i] == need) {
@@ -37,6 +44,6 @@ public class leet15 {
                 }
             }
         }
-
+        return pairs;
     }
 }
