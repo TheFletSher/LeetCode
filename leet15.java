fd74b866441eb8f3a8ef9a0a3ede7ef9f557d26b
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 20:53:07 -0400

    trying hashsets

--- leet15.java
+++ leet15.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class leet15 {
@@ -11,39 +12,21 @@ public class leet15 {
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int right = nums.length - 1;
-        // No negative numbers or zeros
-        if (nums[right] + nums[0] > nums[right]) return new ArrayList<>(0);
+        HashSet<Integer> pos = new HashSet<>(), neg = new HashSet<>();
 
-        List<List<Integer>> pairs = new ArrayList<>();
+        boolean zero = false;
 
-        /*
-         * Follow the left only for as long as it is zero or negative.
-         * Bring the right down until it either the right pairing or move along
-         */
-        for (int left = 0; left < nums.length; left++) {
-            if (nums[left] > 0 || right - left <= 1) break;
-
-            int need = nums[right] - nums[left];
-            if (need > nums[right]) {
-                while (need > nums[right]) {
-                    left++;
-                    if (right <= left) break;
-                    need = nums[right] - nums[left];
-                }
-            } else if (need < nums[left]) {
-                while (need < nums[left]) {
-                    right--;
-                    if (right <= left) break;
-                    need = nums[right] - nums[left];
-                }
-            }
-            int offset = 1;
-            while (right > left) {
-                
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                pos.add(nums[i]);
+            } else if (nums[i] > 0) {
+                neg.add(nums[i]);
+            } else {
+                zero = true;
             }
         }
+
+        List<List<Integer>> pairs = new ArrayList<>();
 
         return pairs;
     }
