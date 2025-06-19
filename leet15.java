c5fd365af1581a5433cab8cb92b8a28f767baee0
TheFletSher <lfhsherson95@gmail.com>
2025-06-18 20:43:24 -0400

    good start 15

--- leet15.java
+++ leet15.java
@@ -3,47 +3,48 @@ import java.util.Arrays;
 import java.util.List;
 
 public class leet15 {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> pairs = threeSum(nums);
+
+        
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int left = 0, right = nums.length - 1;
+        int right = nums.length - 1;
         // No negative numbers or zeros
-        if (nums[right] + nums[left] > nums[right]) return new ArrayList<>(0);
+        if (nums[right] + nums[0] > nums[right]) return new ArrayList<>(0);
 
         List<List<Integer>> pairs = new ArrayList<>();
 
-        // Loop through for pairs
-        while (right - left > 1) {
-            // number to find
-            int need = -1 * (nums[right] - nums[left]);
-            // can't work without duplicates
-            if (need >= nums[right]) {
-                if (right - 1 != left && nums[right - 1] == need) {
-                    pairs.add(new ArrayList<>(List.of(nums[left], nums[right - 1], nums[right])));
-                    while (nums[right] == need) {
-                        right--;
-                        if (right <= left) return pairs;
-                    }
+        /*
+         * Follow the left only for as long as it is zero or negative.
+         * Bring the right down until it either the right pairing or move along
+         */
+        for (int left = 0; left < nums.length; left++) {
+            if (nums[left] > 0 || right - left <= 1) break;
+
+            int need = nums[right] - nums[left];
+            if (need > nums[right]) {
+                while (need > nums[right]) {
+                    left++;
+                    if (right <= left) break;
+                    need = nums[right] - nums[left];
                 }
-                left++;
-            } else if (need <= nums[left]) {
-                if (left + 1 != right && nums[left + 1] == need) {
-                    pairs.add(new ArrayList<>(List.of(nums[left], nums[left + 1], nums[right])));
-                    while (nums[left] == need) {
-                        left++;
-                        if (left >= right) return pairs;
-                    }
-                }
-                right--;
-            // Look for needed number between 
-            } else {
-                for (int i = left; i < right - 1; i++) {
-                    if (nums[i] == need) {
-                        pairs.add(new ArrayList<>(List.of(nums[left], nums[i], nums[right])));
-                        break;
-                    } else if (nums[i] > need) break;
+            } else if (need < nums[left]) {
+                while (need < nums[left]) {
+                    right--;
+                    if (right <= left) break;
+                    need = nums[right] - nums[left];
                 }
             }
+            int offset = 1;
+            while (right > left) {
+                
+            }
         }
+
         return pairs;
     }
 }
