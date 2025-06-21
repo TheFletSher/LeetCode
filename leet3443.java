46f238b7fc987f848a12172eba2b84ad7f1e7fe5
TheFletSher <lfhsherson95@gmail.com>
2025-06-20 20:35:47 -0400

    i misunderstood :(

--- leet3443.java
+++ leet3443.java
@@ -1,5 +1,10 @@
 public class leet3443 {
-    public int maxDistance(String s, int k) {
+    public static void main(String[] args) {
+        String s = "NWSE";
+        System.out.println(maxDistance(s, 1));
+    }
+
+    public static int maxDistance(String s, int k) {
         int n = 0, e = 0, so = 0, w = 0;
 
         for (char c : s.toCharArray()) {
@@ -14,34 +19,44 @@ public class leet3443 {
             }
         }
 
+        System.out.println(n + ", " + so + ", " + e + ", " + w);
+
         if (k == 0) return Math.abs(e - w) + Math.abs(n - so);
 
         // NORTH SOUTH correction
         if (n >= so) {
+            System.out.println("S->N");
             // ALL SOUTH -> NORTH
             if (k >= so) {
+                System.out.println("ALL S->N");
                 n += so;
                 k -= so;
                 so = 0;
             // AS MUCH SOUTH -> NORTH AS POSSIBLE
             } else {
+                System.out.println("SOME S->N");
                 n += k;
                 so -= k;
                 k = 0;
             }
         } else {
+            System.out.println("N->S");
             // ALL NORTH -> SOUTH
             if (k >= n) {
+                System.out.println("ALL N->S");
                 so += n;
                 k -= n;
                 n = 0;
             // AS MUCH NORTH -> SOUTH AS POSSIBLE
             } else {
+                System.out.println("SOME N->S");
                 so += k;
                 n -= k;
                 k = 0;
             }
         }
+        
+        System.out.println(n + ", " + so + ", " + e + ", " + w);
 
         if (k == 0) return Math.abs(e - w) + Math.abs(n - so);
 
